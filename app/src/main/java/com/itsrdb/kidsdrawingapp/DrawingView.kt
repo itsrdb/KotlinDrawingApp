0672d29744783822dbdcc9248a58744fb53f8bd7
Rohit D Borkar <rohitborkar00@gmail.com>
2021-12-16 21:12:00 +0530

    Added custom dialog box incl 3 sizes

--- app/src/main/java/com/itsrdb/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/itsrdb/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.itsrdb.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -28,7 +29,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -52,6 +53,11 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
             mDrawPaint!!.color = mDrawPath!!.color  //Responsible for color
             canvas.drawPath(mDrawPath!!, mDrawPaint!!)  //Responsible for drawing on screen
         }
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
--- app/src/main/java/com/itsrdb/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/itsrdb/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.itsrdb.kidsdrawingapp
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawingView = findViewById<com.itsrdb.kidsdrawingapp.DrawingView>(R.id.drawing_view)
+        drawingView.setSizeForBrush(5.toFloat())
+
+        val brushBtn = findViewById<ImageButton>(R.id.ib_brush)
+        brushBtn.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setTitle("Brush Size")
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        Toast.makeText(this, "button works", Toast.LENGTH_SHORT).show()
+
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_btn)
+        smallBtn.setOnClickListener {
+            val drawingView = findViewById<com.itsrdb.kidsdrawingapp.DrawingView>(R.id.drawing_view)
+            drawingView.setSizeForBrush(5.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val medBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_btn)
+        medBtn.setOnClickListener {
+            val drawingView = findViewById<com.itsrdb.kidsdrawingapp.DrawingView>(R.id.drawing_view)
+            drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_btn)
+        largeBtn.setOnClickListener {
+            val drawingView = findViewById<com.itsrdb.kidsdrawingapp.DrawingView>(R.id.drawing_view)
+            drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
+
     }
 }
