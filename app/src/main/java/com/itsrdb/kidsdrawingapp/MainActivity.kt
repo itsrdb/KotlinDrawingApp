3da05f5693768f55b9af57b30b984db081bbdfdc
Rohit D Borkar <rohitborkar00@gmail.com>
2021-12-21 20:17:49 +0530

    Added background changer button

--- app/src/main/java/com/itsrdb/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/itsrdb/kidsdrawingapp/MainActivity.kt
@@ -1,21 +1,75 @@
 package com.itsrdb.kidsdrawingapp
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.view.Window
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
     private var mImageButtonCurrentPaint: ImageButton? = null
+    private val getResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+                if(data!!.data != null){
+                    val customPhoto = findViewById<ImageView>(R.id.iv_background)
+                    customPhoto.visibility = View.VISIBLE
+                    customPhoto.setImageURI(data.data)
+                }else{
+                    Toast.makeText(this, "Incorrect file type", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    companion object{
+        private const val STORAGE_PERMISSION_CODE = 1
+    }
+
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())){
+            Toast.makeText(this, "Need Permission", Toast.LENGTH_SHORT).show()
+        }
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +77,9 @@ class MainActivity : AppCompatActivity() {
 
         val drawingView = findViewById<com.itsrdb.kidsdrawingapp.DrawingView>(R.id.drawing_view)
         drawingView.setSizeForBrush(5.toFloat())
+
+        val customPhoto = findViewById<ImageView>(R.id.iv_background)
+        customPhoto.visibility = View.INVISIBLE
 
         val ll_paint_colors = findViewById<LinearLayout>(R.id.ll_paint_colors)
         mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton    //Black
@@ -33,6 +90,16 @@ class MainActivity : AppCompatActivity() {
         val brushBtn = findViewById<ImageButton>(R.id.ib_brush)
         brushBtn.setOnClickListener{
             showBrushSizeChooserDialog()
+        }
+
+        val imageBtn = findViewById<ImageButton>(R.id.ib_gallery)
+        imageBtn.setOnClickListener {
+            if(isReadStorageAllowed()){
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                getResult.launch(pickPhotoIntent)
+            }else{
+                requestStoragePermission()
+            }
         }
     }
 
